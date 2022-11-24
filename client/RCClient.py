ae3463922953cc07461335490f9917d206223274
Soumil Datta <soumildatta@gmail.com>
2022-11-24 00:05:03 -0600

    Safety push

--- client/RCClient.py
+++ client/RCClient.py
@@ -7,19 +7,29 @@ import sys
 # from utilities import testPrint
 
 if __name__ == '__main__':
-    server_name = input('Input the server IP address/name: ');
-    server_port = 12001
+    #!!!!!! REINSTATE USER INPUT LATER --- REMOVED FOR TESTING
+    # server_name = input('Input the server IP address/name: ')
+    server_name = 'localhost'
+    #!!!!!! FIX LINES ABOVE 
+    
+    server_port = 12000
 
     # Obtain all items from folder 
+    # current_dir = os.getcwd()
     current_dir = './'
     dir_list = os.listdir(current_dir)
     dir_list.remove('RCClient.py')
     print(dir_list)
 
+    # TODO: Handle folder transfer later
+    # for item in dir_list:
+    #     if os.path.isdir(f'{current_dir}{item}'):
+    #         print(f'{item} is a folder')
+    #     else:
+    #         print(f'{item} is a file')
+
     # sys.path.insert(0, '../utilities')
     # testPrint()
-
-    exit(0)
 
     try:
         client_socket = socket(AF_INET, SOCK_STREAM)
@@ -27,6 +37,53 @@ if __name__ == '__main__':
         print('Successful connection to server ✅')
         
         # TODO: Save server name for later easy access
+
+        #! Number of files to transfer
+        length = str(len(dir_list))
+        client_socket.send(length.encode())
+        #* Receive response
+        response = client_socket.recv(1024).decode()
+        print(response)
+
+        # Send each file over
+        for filename in dir_list:
+            # Send only if a filename is a file and not dir
+            if os.path.isfile(f'{current_dir}{filename}'):                
+                #! Send filename
+                print('sending', filename)
+                msg = f'FILENAME;{filename}'
+                client_socket.send(msg.encode())
+                #* Receive response
+                resonse = client_socket.recv(1024).decode()
+                print(response)
+
+                #! Send file data
+                file = open(filename, 'r')
+                data = file.read()
+                msg = f'CONTENTS;{data}'
+                client_socket.send(msg.encode())
+                #* Receive response
+                response = client_socket.recv(1024).decode()
+                print(response)
+
+                #! Finish sending 
+                msg = f'FINISH;1'
+                client_socket.send(msg.encode())
+                #* Receive response
+                resonse = client_socket.recv(1024).decode()
+                print(response)
+
+                # Send size of file 
+                # filesize = os.stat(filename)
+                # client_socket.send(filesize.encode())
+
+                
+
+                # while data:
+                #     client_socket.send(data)
+                #     data = file.read(1024)
+                # file.close()
+                # client_socket.close()
     except:
         print('Could not connect to server ❌')
         print('Make sure the server name/IP is correct, and the server is running')
--- server/RCServer.py
+++ server/RCServer.py
@@ -3,7 +3,7 @@
 from socket import *
 
 if __name__ == '__main__':
-    server_port = 12001
+    server_port = 12000
     server_socket = socket(AF_INET, SOCK_STREAM)
     server_socket.bind(('', server_port))
     server_socket.listen(1)
@@ -12,3 +12,54 @@ if __name__ == '__main__':
         print('Server is ready to receive file')
 
         connection_socket, addr = server_socket.accept()
+
+        #! Number of files to receive
+        recvLen = connection_socket.recv(1024).decode()
+        length = int(recvLen)
+        #* Send response
+        connection_socket.send(f'FROM SERVER: Received number of files to store ({length})'.encode())
+
+
+        while True:
+            #! Receive item 
+            item = (connection_socket.recv(1024).decode())
+            print(item)
+            try:
+                type, item = item.split(';')
+            except:
+                print('EXCEPTION')
+                print(item)
+            # connection_socket.send(f'FROM SERVER: Receiving file {item}'.encode())
+
+            if type == 'FILENAME':
+                file = open(item, 'w')
+                #* Send resonse
+                connection_socket.send(f'FROM SERVER: Created file {item}'.encode())
+
+            if type == 'CONTENTS':
+                file.write(item)
+                #* Send resonse
+                connection_socket.send(f'FROM SERVER: Writing to file'.encode())
+            
+            if type == 'FINISH':
+                file.close()
+                #* Send resonse
+                connection_socket.send(f'FROM SERVER: {item}'.encode())
+
+            if type == 'CLOSE':
+                connection_socket.send(f'FROM SERVER: FINSHED'.encode())
+                break
+            # filesize = connection_socket.recv(1024).decode()
+            # filesize = int(filesize)
+            # print(filesize)
+
+            # file = open(filename, 'wb')
+
+            # # Get file  
+            # filedata = connection_socket.recv(1024)
+            # while filedata:
+            #     file.write(filedata)
+            #     filedata = connection_socket.recv(1024)
+
+            # file.close()
+        connection_socket.close()

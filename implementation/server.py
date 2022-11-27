4e4b77177fc19e7c56bf2ed0817bf9026a8fb5a2
Soumil Datta <soumildatta@gmail.com>
2022-11-27 12:17:29 -0600

    Change chunk size to 1024

--- implementation/server.py
+++ implementation/server.py
@@ -1,17 +1,17 @@
-import socket
+from socket import *
+import os 
 
-import hashlib
-
-serversock = socket.socket()
-host = socket.gethostname()
+serversock = socket()
+host = gethostname()
 port = 12000
 serversock.bind((host,port))
 filename = ""
 serversock.listen(1)
 print("Waiting for a connection.....")
 
-clientsocket,addr = serversock.accept()
+clientsocket, addr = serversock.accept()
 print("Got a connection from %s" % str(addr))
+
 while True:
     size = clientsocket.recv(16) # Note that you limit your filename length to 255 bytes.
     if not size:
@@ -21,7 +21,8 @@ while True:
     filesize = clientsocket.recv(32)
     filesize = int(filesize, 2)
     file_to_write = open(filename, 'wb')
-    chunksize = 4096
+    
+    chunksize = 1024
     while filesize > 0:
         if filesize < chunksize:
             chunksize = filesize
@@ -31,4 +32,5 @@ while True:
 
     file_to_write.close()
     print('File received successfully')
+
 serversock.close()
